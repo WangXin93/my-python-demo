9939f241e1830bd0f2842f230f03adeeb9c56e9f
WangXin93 <wangxin19930411@163.com>
2018-05-19 22:20:41 +0800

    Add Luhn.py

---
+++ Luhn.py
@@ -0,0 +1,37 @@
+"""
+Luhn algorithm used to verify credit numbers, which is a mutual recursion
+
+From Wikipedia: http://en.wikipedia.org/wiki/Luhn_algorithm
+"""
+def split(n):
+    return n // 10, n % 10
+
+def sum_digits(n):
+    if n < 10:
+        return n
+    else:
+        all_but_last, last = split(n)
+        return sum_digits(all_but_last) + last
+
+def luhn_sum(n):
+    if n < 10:
+        return n
+    else:
+        all_but_last, last = split(n)
+        return luhn_sum_double(all_but_last) + last
+
+def luhn_sum_double(n):
+    """
+    >>> luhn_sum(2)
+    2
+    >>> luhn_sum(32)
+    8
+    >>> luhn_sum(5105105105105100)
+    20
+    """
+    all_but_last, last = split(n)
+    luhn_digit = sum_digits(2 * last)
+    if n < 10:
+        return luhn_digit
+    else:
+        return luhn_sum(all_but_last) + luhn_digit
--- README.md
+++ README.md
@@ -18,6 +18,8 @@ Here will store my python scripts written while daily work.
 
 [credit_card.py](credit_card.py) is a demo to show vertification algorithm of American Express credit card number.
 
+[Luhn.py](Luhn.py) is recursion version demo to verify MasterCard credit card number.
+
 [zero_init.py](zero_init.py)用一个两层神经网络说明了，多层神经网络初始化权重全部为零会使得隐藏层每个神经元的计算结果相同，导致隐藏层多个神经元的作用和一个神经元一样。可以发现w1的每一行为相同数字。
 
 [chart.py](chart.py) can draw a simple bar plot using "ascii art", Very funny:)
