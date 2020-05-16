ec07b794f2feccd923c9a69adb39d9478179b030
WangX <wangxin19930411@163.com>
2020-05-16 14:33:07 +0000

    Add binary search v2

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -41,9 +41,9 @@ def binary_contains(lst, value):
 
 def binary_contains_recursive(lst, value):
     """
-    >>> binary_contains([0, 1, 2, 4, 6, 7, 8], 3)
+    >>> binary_contains_recursive([0, 1, 2, 4, 6, 7, 8], 3)
     False
-    >>> binary_contains([0, 1, 2, 4, 6, 7, 8], 4)
+    >>> binary_contains_recursive([0, 1, 2, 4, 6, 7, 8], 4)
     True
     """
     def _recur(left, right):
@@ -53,7 +53,57 @@ def binary_contains_recursive(lst, value):
         if lst[mid] == value:
             return True
         elif lst[mid] > value:
-            return _iter(left, mid-1)
+            return _recur(left, mid-1)
         elif lst[mid] < value:
-            return _iter(mid+1, right)
+            return _recur(mid+1, right)
     return _recur(0, len(lst)-1)
+
+"""
+Another version of binary search. It only has two branch in the control flow.
+
+Example:
+
+0, 1, 3, 4 find(2) left right  mid
+                    0.   3.     1.  lst[1] = 1 < 2
+                    2.   3.     2.  lst[2] = 3 > 2
+                    2.   2.
+"""
+
+def binary_contains_v2(lst, value):
+    """
+    >>> binary_contains_v2([0, 1, 2, 4, 6, 7, 8], 3)
+    False
+    >>> binary_contains_v2([0, 1, 2, 4, 6, 7, 8], 4)
+    True
+    """
+    left, right = 0, len(lst)-1
+    while left < right:
+        mid = (left + right) // 2
+        if lst[mid] < value:
+            left = mid + 1
+        else:
+            right = mid
+    return lst[left] == value
+
+def binary_contains_recursive_v2(lst, value):
+    """
+    >>> binary_contains_recursive_v2([0, 1, 2, 4, 6, 7, 8], 3)
+    False
+    >>> binary_contains_recursive_v2([0, 1, 2, 4, 6, 7, 8], 4)
+    True
+    """
+    def _recur(left, right):
+        if left < right:
+            mid = (left + right) // 2
+            if lst[mid] < value:
+                return _recur(mid+1, right)
+            else:
+                return _recur(left, mid)
+        else:
+            return lst[left] == value
+    return _recur(0, len(lst)-1)
+
+
+if __name__ == "__main__":
+    print(binary_contains([0, 1, 2, 4, 6, 7, 8], 3))
+    print(binary_contains_v2([0, 1, 2, 4, 6, 7, 8], 3))
