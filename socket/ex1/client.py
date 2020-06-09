f01f6a414a3f742266f5ded563291a64913ef809
Xin Wang <wangxin19930411@163.com>
2020-06-09 13:09:45 +0100

    Update comments on socket

--- socket/ex1/client.py
+++ socket/ex1/client.py
@@ -1,15 +1,24 @@
+""" The client side socket
+"""
 import socket
 
+# Some contants, skip them and read it later
 # IP_ADDRESS = "127.0.0.1"
 IP_ADDRESS = socket.gethostname()
 PORT = 5556
+
+# Create socket with IP address and port
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((IP_ADDRESS, PORT))
 
+# Use while loop to keep receive message from server
 complete_info = ""
 while True:
-    msg = sock.recv(7)
+    # Receive data from server, buffer size 1
+    # It means each step only receive one byte
+    msg = sock.recv(1)
     if len(msg) == 0:
         break
     complete_info += msg.decode("utf-8")
+    print(complete_info)
 print(complete_info)
--- socket/ex1/server.py
+++ socket/ex1/server.py
@@ -5,16 +5,24 @@
 """
 import socket
 
+# Define some constants here, you can skip this paragraph and read it later
 # IP_ADDRESS = "127.0.0.1"
 IP_ADDRESS = socket.gethostname()
 PORT = 5556
+
+# Create a socket, a socket is a combination of IP address and port
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((IP_ADDRESS, PORT))
-sock.listen(5)  # Maximum listen to 5 count
+
+# This socket listen to maximum 5 clients
+sock.listen(5)
 print("Server start at {}:{}".format(IP_ADDRESS, PORT))
 
 while True:
-    conn, adr = sock.accept()
-    print("Connection to {} established.".format(adr))
+    # Client socket is accepted, named conn
+    conn, addr = sock.accept()
+    print("Connection to {} established.".format(addr))
+    # Send string to the client, here send bytes need to be encoded
     conn.send(bytes("Socket Programming in Python", "utf-8"))
+    # If server side socket close, the client side will also close
     conn.close()
